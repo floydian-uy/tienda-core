81a61a01097081beb88d63c28c920c1efadd5cef
Ignacio <floydian.uy@gmail.com>
2016-05-25 21:52:38 -0300

    Rename join tables

--- db/migrate/20160526004016_create_join_table.rb
+++ db/migrate/20160526004016_create_join_table.rb
@@ -1,6 +1,6 @@
 class CreateJoinTable < ActiveRecord::Migration
   def change
-    create_join_table :registries, :products do |t|
+    create_join_table :tienda_registries, :tienda_products do |t|
       t.index [:registry_id, :product_id]
       t.index [:product_id, :registry_id]
     end
