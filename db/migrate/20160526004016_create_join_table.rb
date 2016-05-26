c83199c62068c87ddb40f0bf7f334e9773d7a892
Ignacio <floydian.uy@gmail.com>
2016-05-25 21:58:04 -0300

    Change join table for registries

--- db/migrate/20160526004016_create_join_table.rb
+++ db/migrate/20160526004016_create_join_table.rb
@@ -1,8 +1,8 @@
 class CreateJoinTable < ActiveRecord::Migration
   def change
-    create_join_table :tienda_registries, :tienda_products do |t|
-      t.index [:registry_id, :product_id]
-      t.index [:product_id, :registry_id]
+    create_table :tienda_products_registries do |t|
+      t.index :registry_id
+      t.index :product_id
     end
   end
 end
